1daabedea8e3acf8fb68ab07300c5b6f8014b9c7
Poonam21121991 <106526311+Poonam21121991@users.noreply.github.com>
2022-05-30 17:17:13 +0530

    Update TestRunner.java

--- CucumberProjectTest/src/main/java/MyRunner/TestRunner.java
+++ CucumberProjectTest/src/main/java/MyRunner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
-			features = "C:\\Users\\yadav\\eclipse-workspace\\CucumberProjectTest\\src\\main\\java\\Features\\TestCase.feature", //the path of the feature files
+			features = "src\\main\\java\\Features\\TestCase.feature", //the path of the feature files
 			glue={"StepDefinitions"}, //the path of the step definition files
 			monochrome = true //display the console output in a proper readable format
 			//strict = true, //it will check if any step is not defined in step definition file
